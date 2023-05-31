cfa52dcbfecb5f5530a7f5c9e74eb355ded1ffa1
Seyit İlham Fırtına <38538894+ilhamfi@users.noreply.github.com>
2023-05-31 23:13:26 +0300

    This Java code implements a simple graphical user interface (GUI) using the javax.swing and java.awt libraries.
    A Java program that creates a graphical window using javax.swing and java.awt libraries. The window has a fixed size, displays an image icon, and has a black background.

---
+++ src/java gui/Frame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class Frame extends JFrame {
+
+
+    public static void main(String[] args) {
+       /* JFrame frame = new JFrame();
+        frame.setSize(450, 450);
+
+        frame.setVisible(true);
+        frame.setTitle("try");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon image = new ImageIcon("art.jpg");
+        frame.setIconImage(image.getImage());
+        frame.getContentPane().setBackground(Color.black);
+*/
+        MyFrame k=new MyFrame();
+    }
+}
---
+++ src/java gui/MyFrame.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends Frame {
+MyFrame(){
+
+
+    this.setSize(450, 450);
+
+    this.setVisible(true);
+    this.setTitle("try");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ImageIcon image = new ImageIcon("art.jpg");
+    this.setIconImage(image.getImage());
+    this.getContentPane().setBackground(Color.black);
+
+
+
+
+
+}
+
+
+
+
+
+}
